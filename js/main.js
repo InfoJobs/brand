530be3abd2f3fcdf5ebef1b489b3cd7e0ed95e92
Alex Bailon <alex.bailon@infojobs.net>
2015-03-09 17:44:31 +0100

    new website !!!!!!!

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@
 	
 	
 	//--------------------------------------------------------------------------
+
+	// Columnas ajustadas verticalmente
+
+	function setGridHeight(){
+		$('.grid').each(function(){
+			var height = 0;
+			$(this).find(".box-row").height('');
+			$(this).find(".box-row").each(function(){
+				if(height < $(this).innerHeight())
+					height = $(this).innerHeight();
+			});
+			$(this).find(".box-row").height(height);
+			$(this).find(".card-overflow").height(height);
+		});
+	}
+	$(w).resize(setGridHeight);
+
+
 	$(d).ready(function() {
 		
 		$('.js-download').on('click', function(event) {
@@ -17,7 +35,36 @@
 				.removeClass('fade-up')
 				.addClass('fade-down');
 		});
+		// open-hover-card
 
+		$('.open-hover-card').on('click', function(event) {
+			event.preventDefault();
+			$(this).parent().parent().parent()
+				.addClass('open-hover');
+		});
+		$('.close_card_hover').on('click', function(event) {
+			event.preventDefault();
+			$(this).parent().parent().parent()
+				.removeClass('open-hover');
+		});
+		$('.voiceandtone_nav a').on('click', function(event) {
+			var self = this;
+
+			event.preventDefault();
+			$('.voiceandtone_nav a.active').removeClass("active");
+			$(this).addClass("active");
+
+			scrollToElement(_("_voicetone"));
+
+			$('.voice' + this.dataset.voice).prependTo($('.voice' + this.dataset.voice).parent());
+			setTimeout(function(){
+				$('.voice').removeClass("open-voice");
+				$('.voice' + self.dataset.voice).addClass("open-voice");
+				setGridHeight();
+			},10);
+		});
+
+		setGridHeight();
 	});
 
 
@@ -71,7 +118,7 @@
 		STE_scroll,
 		STE_initial_scroll,
 		STE_times = [],
-		STE_margin_top = 80; // Height of header
+		STE_margin_top = 90; // Height of header
 
 	function scrollToElement(toElement) {
 	    var toPosition = toElement.offsetTop - STE_margin_top,
@@ -113,7 +160,7 @@
 	    var height = _get.WindowHeight(),
 	    	scroll = _get.Scroll(),
 	    	fix = (height - 40) > scroll,
-	    	SE = ['_manual', '_logos', '_icons', '_android', '_templates', '_kit'],
+	    	SE = ['_manual', '_logos', '_icons', '_templates', '_imagenes', '_voicetone'],
 	    	position = 0;
 
 	    for (var k in SE) {
@@ -125,6 +172,16 @@
 	    };
 	}
 
+	function setVoiceToneFixed() {
+	    var height = _get.WindowHeight(),
+	    	scroll = _get.Scroll(),
+	    	limit  = _("_voicetone");
+
+	    var isFixed = scroll >= (limit.offsetTop + 10 - STE_margin_top);
+
+	    changeClassName(_query('.voiceandtone_nav')[0], 'fixed', !isFixed);
+	}
+
 	function changeClassName(o, className, remove) {
 	    o.className = o.className.split(' ' + className).join('');
 	    if (!remove) o.className += ' ' + className;
@@ -134,9 +191,14 @@
 
 	_addEvent(w, 'scroll', function(e) { // On Scroll
 	    setHeaderFixed();
+	    setVoiceToneFixed();
+	});
+	_addEvent(w, 'load', function(e) { // On Scroll
+	    setGridHeight();
+	    setTimeout(setGridHeight, 600);
 	});
 
-	var aLinks = ['top','manual','logos','icons','android','templates','kit'];
+	var aLinks = ['top','manual','logos','icons','templates','imagenes','voicetone'];
 	for( var i in aLinks ) {
 		_(aLinks[i]).addEventListener('click', function(e){
 			e.preventDefault();
